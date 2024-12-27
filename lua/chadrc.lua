9beb85d3720dbce5e0f16d27f56a8f72bb64cad6
lubasinkal <lubasinkal@outlook.com>
2024-12-27 08:25:06 +0200

    Add Nvdash startup configuration with custom header and buttons

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -12,9 +12,48 @@ M.base46 = {
 	-- 	Comment = { italic = true },
 	-- 	["@comment"] = { italic = true },
 	-- },
+	nvdash = {
+		load_on_startup = true,
+		header = {
+			
+			"██╗     ███████╗███╗   ██╗",
+			"██║     ██╔════╝████╗  ██║",
+			"██║     ███████╗██╔██╗ ██║",
+			"██║     ╚════██║██║╚██╗██║",
+			"███████╗███████║██║ ╚████║",
+			"╚══════╝╚══════╝╚═╝  ╚═══╝",
+			"     LUBASI  KAL        ",
+		},                          
+	
+
+	
+	buttons = {
+	  { txt = "  Find File", keys = "ff", cmd = "Telescope find_files" },
+	  { txt = "  Recent Files", keys = "fo", cmd = "Telescope oldfiles" },
+	  { txt = "󰈭  Find Word", keys = "fw", cmd = "Telescope live_grep" },
+	  { txt = "󱥚  Themes", keys = "th", cmd = ":lua require('nvchad.themes').open()" },
+	  { txt = "  Mappings", keys = "ch", cmd = "NvCheatsheet" },
+	  { txt = "  Config file", keys = "c", cmd = ":e $MYVIMRC" },  -- Added button to open config folder
+	  { txt = "  Config folder", keys = "cf", cmd = ":cd $LOCALAPPDATA\\nvim | e init.lua" },
+	  { txt = "─", hl = "NvDashFooter", no_gap = true, rep = true },
+	
+	  {
+		txt = function()
+		  local stats = require("lazy").stats()
+		  local ms = math.floor(stats.startuptime) .. " ms"
+		  return "  Loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms
+		end,
+		hl = "NvDashFooter",
+		no_gap = true,
+	  },
+	
+	  { txt = "─", hl = "NvDashFooter", no_gap = true, rep = true },
+	},
 
 }
 
-M.ui = require("custom.chadrc")
+}
+
+-- M.ui = require("custom.chadrc")
 
 return M
